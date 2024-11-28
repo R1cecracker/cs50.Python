d56470dd7c0191e932d069c025572cbb7e3cfe0f
R1cecracker <troy.harcoan0@gmail.com>
2024-11-29 08:45:59 +1100

    added faces.py and einstein.py from week 0 problem set

---
+++ einstein.py
@@ -0,0 +1,8 @@
+# prompts the user for their input with "m:"
+response = int(input("m: "))
+
+# multiplies the user's result with 300,000,000
+result = response * (300000000 ** 2)
+
+# prints new output with "E:"
+print(f"E: {result}")
---
+++ faces.py
@@ -0,0 +1,5 @@
+# replaces the analogue faces with actual emoji's in the user input
+response = input().replace(":)", "🙂").replace(":(","🙁")
+
+# prints new output
+print(response)
